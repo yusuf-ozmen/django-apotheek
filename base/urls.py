5022120373bc73bfc4921c632f0ca72728ce7bf5
Yusuf <1058297@hr.nl>
2023-07-05 14:56:51 +0200

    Created html, urls and views

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
+    path("", include('django.contrib.auth.urls')),
     path("test/", views.test, name="test"),
+    path("register/", views.register, name="register"),
+    path("profile/", views.edit_profile, name="profile"),
+    path("collection/", views.collection, name="collection"),
+    path('collection/<int:collection_id>/sign_collected/', views.sign_collected, name='sign_collected'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,66 @@
-from django.shortcuts import render
+import datetime
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import Profile, Medicine, Collection
+from .forms import NameForm, MedicineForm, CollectionForm, ProfileForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 # Create your views here.
 
 def test(request):
     return HttpResponse("test");
+
+def index(request):
+    return render(request, "base/index.html")
+
+def register(requests):
+    if requests.method == "POST":
+        form = UserCreationForm(requests.POST)
+        if form.is_valid():
+            user = form.save()
+            login(requests, user)
+            return redirect("index")
+    else:
+        form = UserCreationForm()
+    
+    context = {"form": form}
+    return render(requests, "registration/register.html", context)
+
+@login_required
+def edit_profile(request):
+    profile = Profile.objects.get(user=request.user)
+    
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully")
+            return redirect("profile")
+    else:
+        form = ProfileForm(instance=profile)
+    
+    context = {"form": form}
+    return render(request, "base/profile.html", context)
+
+@login_required
+def collection(request):
+    user = request.user
+    collections = Collection.objects.filter(user=user)
+    return render(request, 'base/collection.html', {'collections': collections})
+
+@login_required
+def sign_collected(request, collection_id):
+    collection = Collection.objects.get(pk=collection_id)
+
+    if collection.collectedapproved:
+        collection.collected = True
+        collection.save()
+        messages.success(request, 'Medicine marked as collected.')
+    else:
+        messages.error(request, 'Medicine must first be approved by an admin.')
+
+    return redirect('collection')
