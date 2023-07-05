b62a01438274e49845d18f3ecdc8a21ce8de377a
Yusuf <1058297@hr.nl>
2023-07-05 14:55:14 +0200

    Fixed home url

--- apotheek/settings.py
+++ apotheek/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
